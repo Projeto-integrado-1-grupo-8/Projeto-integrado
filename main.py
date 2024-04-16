5a446c54c134cf1bd4fc7fe23695ab0314ed7502
blackwolf449 <tacvini@gmail.com>
2024-04-15 22:55:08 -0300

    table update

--- main.py
+++ main.py
@@ -44,19 +44,34 @@ def createProduto():
 
             os.system("cls")
 
-            
-            print("Descrição            |" + " valor  |" + " %")
-            print("Preço de venda  | " + str(produto.precoVenda) + " | 100%")
-            print("Custo do produto    | " + str(produto.custoProduto) + " | " + str(100 * (produto.custoProduto / produto.precoVenda)) + "%")
-            print("Receita Bruta  | " + str(produto.receitaBruta) + " | " + str(100 * (produto.receitaBruta / produto.precoVenda))+ "%")
-            print("Custo Administrativo/Fixo    | " + str(produto.custoAdministrativo) + " | " + str(100 * (produto.custoAdministrativo / produto.precoVenda))+ "%")
-            print("Comissão de vendas    | " + str(produto.comissaoVendas) + " | " + str(100 * (produto.comissaoVendas / produto.precoVenda))+ "%")
-            print("Impostos     | " + str(produto.impostos) + " | " + str(100 * (produto.impostos / produto.precoVenda))+ "%")
-            print("Outros custos   | " + str(produto.outrosCustos) + " | " + str(100 * (produto.outrosCustos / produto.precoVenda))+ "%")
-            print("Rentabilidade     | " + str(produto.receitaBruta - produto.outrosCustos) + " | " + str(100 * ((produto.receitaBruta - produto.outrosCustos) / produto.precoVenda))+ "%")
-            print("Classificação de lucro  | " + str(produto.classificacaoRentabilidade))
 
-            continuar = input("1-continuar\nOutros-Voltar ao menu\n\nDigite a sua escolha: ")
+            table = [['Descricao', 'Valor', '%'], ['Preço de venda', round(produto.precoVenda, 2), '100%'], 
+                    ['Custo de aquisição', round(produto.custoProduto, 2), str(round(100 * (produto.custoProduto / produto.precoVenda), 2))+ "%" ], 
+                    ['Receita bruta', round(produto.receitaBruta, 2), str(round(100 * (produto.receitaBruta / produto.precoVenda), 2))+ "%"], 
+                    ['Custo fixo/administrativo', round(produto.custoAdministrativo, 2), str(round(100 * (produto.custoAdministrativo / produto.precoVenda), 2))+ "%"],
+                    ['Comissão de vendas', round(produto.comissaoVendas, 2), str(round(100 * (produto.comissaoVendas / produto.precoVenda), 2))+ "%"],
+                    ['Imposto', round(produto.impostos, 2), str(round(100 * (produto.impostos / produto.precoVenda), 2))+ "%"],
+                    ['Outros custos', round(produto.outrosCustos, 2), str(round(100 * (produto.outrosCustos / produto.precoVenda), 2))+ "%"],
+                    ['Rentabilidade', round(produto.receitaBruta - produto.outrosCustos,2), str(round(100 * ((produto.receitaBruta - produto.outrosCustos) / produto.precoVenda), 2))+ "%"],
+                    ['Classificação de lucro', produto.classificacaoRentabilidade, '']]
+
+            for item in table:
+                print("|",item[0]," "*(25-len(str(item[0]))),"|",item[1]," "*(25-len(str(item[1]))),"|", item[2]," "*(10-len(str(item[2]))),"|")
+            
+
+            
+            # print("Descrição            |" + " valor  |" + " %")
+            # print("Preço de venda  | " + str(produto.precoVenda) + " | 100%")
+            # print("Custo do produto    | " + str(produto.custoProduto) + " | " + str(100 * (produto.custoProduto / produto.precoVenda)) + "%")
+            # print("Receita Bruta  | " + str(produto.receitaBruta) + " | " + str(100 * (produto.receitaBruta / produto.precoVenda))+ "%")
+            # print("Custo Administrativo/Fixo    | " + str(produto.custoAdministrativo) + " | " + str(100 * (produto.custoAdministrativo / produto.precoVenda))+ "%")
+            # print("Comissão de vendas    | " + str(produto.comissaoVendas) + " | " + str(100 * (produto.comissaoVendas / produto.precoVenda))+ "%")
+            # print("Impostos     | " + str(produto.impostos) + " | " + str(100 * (produto.impostos / produto.precoVenda))+ "%")
+            # print("Outros custos   | " + str(produto.outrosCustos) + " | " + str(100 * (produto.outrosCustos / produto.precoVenda))+ "%")
+            # print("Rentabilidade     | " + str(produto.receitaBruta - produto.outrosCustos) + " | " + str(100 * ((produto.receitaBruta - produto.outrosCustos) / produto.precoVenda))+ "%")
+            # print("Classificação de lucro  | " + str(produto.classificacaoRentabilidade))
+
+            continuar = input("\n\n\n1-continuar\nOutros-Voltar ao menu\n\nDigite a sua escolha: ")
             utilizandoCriacaoDeProduto = continuar == "1"
 
         except ValueError:
